a404d6df9e311fd1d09a9b856d878cf729b05dec
Juan Contreras <tunoooox@hotmail.com>
2019-01-07 02:50:09 -0500

    AVANCE
    solucion bug prop

---
+++ FrontEnd/app/src/sections/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+View,
+Text,
+Image,
+StyleSheet
+} from 'react-native';
+
+function Header(props){
+    return(
+    <View>
+         <Text>este es el header</Text>
+
+    </View>
+
+    )
+}
+
+export default Header;
