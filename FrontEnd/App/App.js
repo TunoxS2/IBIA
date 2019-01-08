39c002ed055ec8e4ee3849f1aa6139a2ea2d8e83
Juan Contreras <tunoooox@hotmail.com>
2019-01-07 23:30:47 -0500

    .

--- FrontEnd/App/App.js
+++ FrontEnd/App/App.js
@@ -1,33 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Alert,Platform, AppRegistry,StyleSheet, Text, View, Button} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class Touchables extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.buttonContainer}>
+         <Button             
+          onPress={this._onPressButton}
+          title="Press Me" 
+          />  
+        </View>
       </View>
     );
   }
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
@@ -47,3 +37,5 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+AppRegistry.registerComponent('App', () => ButtonBasics);
