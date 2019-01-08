ff0e63aa6a28bd395d39b92b3c046829573fc510
Juan Contreras <tunoooox@hotmail.com>
2019-01-08 04:59:02 -0500

    buscador e imagenes funcionando
    listo para hacer 1ra ventana con botones que agan eventos

--- FrontEnd/App/App.js
+++ FrontEnd/App/App.js
@@ -8,7 +8,9 @@ View,
 TouchableOpacity,
 TextInput,
 Dimensions,
-Image
+Image,
+ScrollView,
+WebView
 } from 'react-native';
 
 export default class Touchables extends Component {
@@ -17,37 +19,52 @@ constructor(props, env){
   super(props, env);
 
   this.state = {
-    colorFondo: 'blue'
+    colorFondo: 'blue',
+    direccion: 'http://google.com'
   }
 }
-
     cambiarColor(){
 
     this.setState({colorFondo: this.state.text})
     }
 
+    cambiarPagina(){
+      this.setState({direccion: this.state.text})
+    }
+
   render() {
     return (
-      <View style={{
-    flex: 1,
-     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'yellow',
-  }}>
-        <Image
-          style={{width: 400, height: 400}}
-          source={{uri: 'https://www.rizog.com/wp-content/uploads/2017/02/Dise%C3%B1o-del-b%C3%BAho-dise%C3%B1os-para-mujeres-1.jpg'}}
-        />
-      <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1, width: (Dimensions.get('window').width * 80 / 100),margin: 15,}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
+      <ScrollView>
+        <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: this.state.colorFondo,
+      }}>
+          <Image
+            style={{width: 500, height: 500}}
+            source={{uri: 'https://www.rizog.com/wp-content/uploads/2017/02/Dise%C3%B1o-del-b%C3%BAho-dise%C3%B1os-para-mujeres-1.jpg'}}
+          />
+          <WebView
+            source={{uri: this.state.direccion}}
+            style={{width: Dimensions.get('window').width, height: 500, backgroundColor: 'white'}}
+          />
+        <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, width: (Dimensions.get('window').width * 80 / 100),margin: 15,}}
+            placeholder="Type here to translate!"
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+          />
 
-      <TouchableOpacity style={styles.boton}  onPress={this.cambiarColor.bind(this)}>
-        <Text style={styles.textoBoton}>cambiar color</Text>
-      </TouchableOpacity>
-      </View>
+          <TouchableOpacity style={styles.boton}  onPress={this.cambiarColor.bind(this)}>
+          <Text style={styles.textoBoton}>cambiar pagina</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.boton}  onPress={this.cambiarColor.bind(this)}>
+          <Text style={styles.textoBoton}>cambiar color</Text>
+          </TouchableOpacity>
+          </View>
+       </ScrollView>
     );
   }
 } 
