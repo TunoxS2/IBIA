7e08689ac4314127fdfdcf6a9bf505828134c873
Juan Contreras <tunoooox@hotmail.com>
2019-01-08 03:08:34 -0500

    imágenes ingresadas servidor
    avance imagines sin error

--- FrontEnd/App/App.js
+++ FrontEnd/App/App.js
@@ -1,19 +1,52 @@
 import React, {Component} from 'react';
-import { Alert,Platform, AppRegistry,StyleSheet, Text, View, Button} from 'react-native';
+import { Alert,
+Platform,
+AppRegistry,
+StyleSheet,
+Text, 
+View, 
+TouchableOpacity,
+TextInput,
+Dimensions,
+Image
+} from 'react-native';
 
 export default class Touchables extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
+
+constructor(props, env){
+  super(props, env);
+
+  this.state = {
+    colorFondo: 'blue'
   }
+}
+
+    cambiarColor(){
+
+    this.setState({colorFondo: this.state.text})
+    }
+
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-         <Button             
-          onPress={this._onPressButton}
-          title="Press Me" 
-          />  
-        </View>
+      <View style={{
+    flex: 1,
+     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+  }}>
+        <Image
+          style={{width: 400, height: 400}}
+          source={{uri: 'https://www.rizog.com/wp-content/uploads/2017/02/Dise%C3%B1o-del-b%C3%BAho-dise%C3%B1os-para-mujeres-1.jpg'}}
+        />
+      <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1, width: (Dimensions.get('window').width * 80 / 100),margin: 15,}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+
+      <TouchableOpacity style={styles.boton}  onPress={this.cambiarColor.bind(this)}>
+        <Text style={styles.textoBoton}>cambiar color</Text>
+      </TouchableOpacity>
       </View>
     );
   }
@@ -36,6 +69,21 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+boton: {
+  backgroundColor: 'purple',
+  width: 300,
+  height: 50,
+  borderWidth: 2,
+  borderColor: 'black',
+  borderRadius: 15,
+  alignItems: 'center',
+  flexDirection: 'row'
 
-AppRegistry.registerComponent('App', () => ButtonBasics);
+},
+textoBoton:{
+  color: 'white',
+  fontSize: 20,
+  textAlign: 'center',
+  flex: 1
+}
+});
