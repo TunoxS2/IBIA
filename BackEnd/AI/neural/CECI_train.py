ef777bd4288536251368295f55e984e67fea8ed4
Juan Contreras <tunoooox@hotmail.com>
2019-01-11 04:07:11 -0500

    comentarios para el manejo del archivo del modelo entrenado (sklearn)

--- BackEnd/AI/neural/CECI_train.py
+++ BackEnd/AI/neural/CECI_train.py
@@ -22,6 +22,19 @@ my_classifier.fit(X_train, y_train)
 filename = "ceci_clf.sav"
 pk.dump(my_classifier, open(filename, 'wb'))
 
+# DESCOMENTAR ESTAS LINEAS PARA HACER LA PREDICCION, O COPIARLAS RESPECTIVAMENTE
 #loaded_model = pk.load(open(filename, 'rb'))
 #predictions = loaded_model.predict(X_test)
-#print(accuracy_score(y_test, predictions))
+#print(int(predictions[0]))
+
+
+
+# SOLO DESCOMENTAR EN CASO DE TESTING (VER LA VERACIDAD DEL ALGORITMO)
+#print("CECI PREDIJO CON UN: ",float(accuracy_score(y_test, predictions))*100,"% de precision")
+
+
+#FORMATO DEL X_TEST =
+# [NOMBRE,CEDULA,EMAIL,PASSWORD,CONTADOR,HORA 1,HORA 2,HORA 3,HORA 4,HORA 5,
+#HORA 6,HORA 7,HORA 8,HORA 9,HORA 10,HORA 11,HORA 12,HORA 13,HORA 14,HORA 15,
+#HORA 16,HORA 17,HORA 18,PROM 1,PROM 2,PROM 3,PROM 4,PROM 5,PROM 6,PROM 7,PROM 8,
+#PROM 9,ANALISIS]
